1f37a6839c1a347aae322225d5691bb42d85726d
vertex <778950965@qq.com>
2020-10-11 22:33:15 +0800

    half lucky half bad! a little nueural network test just pass away. I need to do more specific real environment simulation to determine the final model

--- Pizeo/ManArray.m
+++ Pizeo/ManArray.m
@@ -1,12 +1,14 @@
 classdef ManArray
     properties
         Value
+        lamp_range
     end
     methods
-        function obj = ManArray(F, map_boundary)
+        function obj = ManArray(F, map_boundary, lamp_range)
             if nargin ~= 0
                 for i = 1:F
                     obj(i).Value = Man(map_boundary);
+                    obj(i).lamp_range = lamp_range;
                 end
             end
         end
@@ -45,11 +47,38 @@ classdef ManArray
             map_array(get_index) = map_array(get_index)+position_array(:,3);
             
         end
+
+        function map_array = get_man(obj)
+            maplength = obj(1).Value.map_boundary;
+            num_Man = length(obj);
+            position_array = [];
+            for n = 1:num_Man
+                position_array(n,:) = obj(n).Value.position;
+            end
+            map_array = zeros(maplength);
+            get_index = position_array(:,1)+(position_array(:,2)-1)*maplength;
+            map_array(get_index) = 1;
+        end
         
-        function sample(obj)
-            map_array = get_pressure(obj);
+        function [lamp_regions_final, lamp_regions_label] = sample(obj)
+            map_array = get_man(obj);
 
+            %divide the whole preesure to several region
+            N = obj(1).lamp_range*ones(1,obj(1).Value.map_boundary/obj(1).lamp_range);
+            lamp_regions = mat2cell(map_array, N, N);
 
+            lamp_regions = lamp_regions(:);
+            len_regions = length(lamp_regions);
+            lamp_regions_label = zeros(len_regions,1);
+            for n = 1:len_regions
+                if sum(lamp_regions{n},'all')>0
+                    lamp_regions_label(n) = 1;
+                end
+            end
+            lamp_regions_final = [];
+            for n = 1:len_regions
+                lamp_regions_final(n, :) = lamp_regions{n}(:);
+            end
         end
     end
 end
--- Pizeo/simulator.m
+++ Pizeo/simulator.m
@@ -1,12 +1,24 @@
 num_agent = 5;
 map_boundary = 40;
-a = ManArray(num_agent, map_boundary);
+lamp_range = 5;
+a = ManArray(num_agent, map_boundary, lamp_range);
 
-figure
-colormap('gray');
-axis equal;
-while true
-    a.visualize;
+lamp_x = [];
+lamp_y = [];
+
+% draw codes
+% figure
+% colormap('gray');
+% axis equal;
+counter = 1000;
+while counter>0
+    % draw codes
+    % a.visualize;
+
     a.move
-    drawnow
+    [temp_x temp_y] = a.sample;
+    lamp_x = [lamp_x;temp_x];
+    lamp_y = [lamp_y;temp_y];
+    counter = counter-1;
+    % drawnow
 end
